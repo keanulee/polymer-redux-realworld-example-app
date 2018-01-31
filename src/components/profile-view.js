4f2840f67d621418dbda07c2e6d406811cefbf51
Keanu Lee <keanulee@google.com>
2018-01-31 12:31:51 -0800

    Fix profile-view

--- src/components/profile-view.js
+++ src/components/profile-view.js
@@ -1,16 +1,16 @@
 import { Element as PolymerElement } from '../../node_modules/@polymer/polymer/polymer-element.js';
 import '../../node_modules/@polymer/polymer/lib/elements/dom-repeat.js';
-import articles, { articlesSelector } from '../reducers/articles.js';
+import articleList, { articlesSelector } from '../reducers/articleList.js';
 import profile, { profileSelector, usernameSelector } from '../reducers/profile.js';
 import { store } from '../store.js';
 import { fetchProfile } from '../actions/profile.js';
-import { fetchArticles } from '../actions/articles.js';
+import { fetchArticles } from '../actions/articleList.js';
 import { connect } from '../../lib/connect-mixin.js';
 import { sharedStyles } from './shared-styles.js';
 import { pageIndexSelector } from '../reducers/location.js';
 
 store.addReducers({
-  articles,
+  articleList,
   profile
 });
 
